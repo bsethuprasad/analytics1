13c32b1f62ed0a9475467ae70fcce85931d35360
bsethuprasad <sprasadp1998@gmail.com>
2018-10-28 09:19:21 +0530

    ds

---
+++ chaidclust.R
@@ -0,0 +1,42 @@
+#CHAID - dataset USvote #multisplit
+
+# require(rsample) # for dataset and splitting also loads broom and tidyr
+
+install.packages("CHAID", repos="http://R-Forge.R-project.org")
+
+library(CHAID) 
+#library for performing CHAID decision tree
+
+#used when all variables are categories
+
+#Dataset
+
+
+la=data(USvote)  #from lib CHAID
+la
+?USvote
+
+head(USvote)
+
+str(USvote)
+usvote
+#Quick CHAID analysis
+
+set.seed(101)
+usvote
+sample1 = USvote[sample(1:nrow(USvote), 1000),]
+ncol(sample1)
+length(sample1)
+chaidModel = chaid(vote3 ~ ., data = sample1, control=chaid_control(minbucket = 20, minsplit=7, minprob=0))
+
+#minsplit	Number of observations in splitted response at which no further split is desired.
+
+#minbucket Minimum number of observations in terminal nodes.
+
+#minprob	Mininimum frequency of observations in terminal nodes.
+
+print(chaidModel)
+
+plot(chaidModel)
+
+?chaid
